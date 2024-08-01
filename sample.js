101726c972c879b661a911a05a61d8e4a691bdad
Teja <tejasimma033@gmail.com>
2024-08-02 00:35:02 +0530

    sample and list

--- sample.js
+++ sample.js
@@ -2,30 +2,55 @@
 let elementId = document.getElementById("list");
 elementId.innerHTML = "teja"
 
-elementId.requestPointerLock();
-
-const con = document.createElement('names');
-con.innerHTML="teja"
-document.body.appendChild(con);
-
 let elementclass = document.getElementsByClassName("classname")
 elementclass[0].innerHTML = "siva"
-
 
 let bytag = document.getElementsByTagName("div")
 bytag[0].innerHTML = "srkr"
 
 let queryall = document.querySelectorAll(".classname")[0]
-queryall.innerHTML = "cse"
+// queryall.innerHTML = "cse"
+// queryall.textContent="cse"
 
-// let query = document.querySelector("#list")
+let query = document.querySelector("#list")
 // query.innerHTML = localStorage.getItem("name")
+query.textContent="hello darlings.."
 
+const con = document.createElement('names');
+con.innerHTML="teja"
+document.body.appendChild(con);
 
+queryall.setAttribute('class','newclass')
 
-function Speak() {
+// query.removeAttribute('id')
+
+console.log(query.getAttribute('id'))
+
+queryall.textContent="cse deprt"
+
+queryall.style.fontSize="50px"
+
+con.style.color="blue"
+
+reminder=document.getElementById("alert")
+reminder.addEventListener('click',Alert)
+// reminder.removeEventListener('click',Alert)
+
+// add class elements
+elementclass[0].classList.add('addclass')
+elementclass[0].style.color="red"
+document.getElementsByClassName('addclass')[0].textContent="add class";
+
+function Alert() {
+    // alert("hello darling..")
+    elementclass[0].classList.toggle('addclass')
+}
+
+function Speak(){
     Speech()
 }
+
+elementId.requestPointerLock();
 
 const removeName = () => {
     localStorage.removeItem("name")
@@ -38,3 +63,4 @@ const getLoc = () => {
 const Canva = () => {
     Canvas()
 }
+
